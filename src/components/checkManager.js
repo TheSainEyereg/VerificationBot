b006104f050731d3e27adfa3b3085feb28a674bf
TheSainEyereg <oleglogwinow@gmail.com>
2023-05-29 01:29:16 +0300

    Update checkManager.js

--- src/components/checkManager.js
+++ src/components/checkManager.js
@@ -2,16 +2,26 @@ const { roles } = require("../config");
 const { GuildMember, PermissionFlagsBits } = require("discord.js");
 
 /**
+ * @typedef {"administrator"|"moderator"|"inspector"|"user"} AccessType
+ */
+
+/**
  * 
  * @param {GuildMember} member 
- * @param {"administrator"|"moderator"|"inspector"|"user"} type 
+ * @param {AccessType | Array.<AccessType>} types 
  * @returns {Boolean}
  */
-const hasAccess = (member, type) =>  
-	type === "user" || // Allowed for all
-	member.permissions.has(PermissionFlagsBits.Administrator) || // Administrator can do anything!
-	type === "moderator" && member.roles.cache.has(roles.moderator) || // moderator != inspector
-	type === "inspector" && member.roles.cache.has(roles.inspector); // inspector != moderator
+const hasAccess = (member, types) => {
+	for (const type of typeof types === "string" ? [types] : types) {
+		if (
+			type === "user" || // Allowed for all
+			member.permissions.has(PermissionFlagsBits.Administrator) || // Administrator can do anything!
+			type === "moderator" && member.roles.cache.has(roles.moderator) || // moderator != inspector
+			type === "inspector" && member.roles.cache.has(roles.inspector) // inspector != moderator
+		) return true;
+	}
+	return false;
+}
 
 
 module.exports = { hasAccess };
