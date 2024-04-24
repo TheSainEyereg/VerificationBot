12c5307ab3c4fcef694a2fb966e23d0b287bfe1c
TheSainEyereg <oleglogwinow@gmail.com>
2024-04-24 20:55:04 +0300

    Get info by nickname

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,50 +1,71 @@
-const { Interaction, EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+const { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 const { roles } = require("../config");
 const { Colors } = require("../components/constants");
-const { getUser } = require("../components/dataManager");
-const { warning } = require("../components/messages");
+const { getUser, getUserByName } = require("../components/dataManager");
 const { hasAccess } = require("../components/checkManager");
+const { warning } = require("../components/messages");
 
 module.exports =  {
-	data: new SlashCommandBuilder().setName("info").setDescription("Получает дату регистрации пользователя Discord.").addUserOption(option =>
-		option
-			.setName("target")
-			.setDescription("Пользователь")
-			.setRequired(false)
-	),
+	data: new SlashCommandBuilder().setName("info").setDescription("Получает дату регистрации пользователя Discord.")
+		.addUserOption(option =>
+			option
+				.setName("member")
+				.setDescription("Участник сервера")
+				.setRequired(false)
+		)
+		.addStringOption(option =>
+			option
+				.setName("nickname")
+				.setDescription("Ник в игре")
+				.setRequired(false)
+		),
 	access: "user",
 	/**
 	 * Interaction
 	 * @param {ChatInputCommandInteraction} interaction - interaction
 	 */
 	async execute(interaction) {
-		const member = interaction.options.getMember("target") || interaction.member;
+		const target = interaction.options.getMember("member") ?? interaction.member;
+		const nickname = interaction.options.getString("nickname");
 
 		const advancedOutput = hasAccess(interaction.member, ["inspector", "moderator"]);
 
-		const possibleUser = getUser(member.id);
+		const possibleUser = nickname ? getUserByName(nickname) : getUser(target.id);
+
+		if (nickname && !possibleUser) return interaction.editReply({
+			embeds: [
+				warning(null, "Не найдено!", "Данный ник не принадлежит и не принадлежал ни одному игроку!", {embed: true})
+			]
+		});
+
+		const member = nickname ? await interaction.guild.members.cache.get(possibleUser.userId) : target;
+		const user = nickname ? await interaction.client.users.fetch(possibleUser.userId).catch(() => null) : member.user;
 
 		interaction.editReply({
 			embeds: [
 				new EmbedBuilder({
 					color: Colors.Regular,
-					title: "Информация о пользователе "+member.displayName,
+					title: `Информация о пользователе ${member?.displayName ?? user?.username ?? ""}`,
 					fields: [
 						{
 							name: "Discord ID",
-							value: `\`${member.user.id}\``,
+							value: `\`${possibleUser?.userId}\``,
 							inline: true
 						},
-						{
-							name: "Discord имя и тег",
-							value:`\`${member.user.username + (member.user.discriminator !== "0" ? `#${member.user.discriminator}` : "")}\``,
-							inline: true
-						},
-						{
-							name: possibleUser ? "Впервые вошел на сервер" : "Дата захода",
-							value: `<t:${Math.floor((possibleUser?.firstJoined ? possibleUser.firstJoined : member.joinedTimestamp) / 1000)}>`,
-						},
-						... member.roles.cache.has(roles.approved) ? [
+						... user ? [
+							{
+								name: "Discord имя и тег",
+								value:`\`${user.username + (user.discriminator !== "0" ? `#${user.discriminator}` : "")}\``,
+								inline: true
+							}
+						] : [],
+						... (member || possibleUser) ? [
+							{
+								name: possibleUser ? "Впервые вошел на сервер" : "Дата захода",
+								value: `<t:${Math.floor((possibleUser?.firstJoined ? possibleUser.firstJoined : member.joinedTimestamp) / 1000)}>`,
+							}
+						] : [],
+						... (possibleUser?.name || member.roles.cache.has(roles.approved)) ? [
 							{
 								name: "Ник в игре",
 								value: possibleUser?.name ? `\`${possibleUser.name}\`` : "❌ Не установлен!",
@@ -90,7 +111,7 @@ module.exports =  {
 					]
 				})
 			],
-			... advancedOutput && possibleUser.answers && {
+			... advancedOutput && possibleUser?.answers && {
 				files: [{
 					name: "answers.txt",
 					attachment: Buffer.from(JSON.parse(possibleUser.answers).map(qa => `Вопрос: ${qa.q}\nОтвет: ${qa.a}\n\n`).join(""))
